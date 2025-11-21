cba5d650a8389f2e3dca5a33e0f2f586c87fc844
RedKnightM <rkatkori@ucsc.edu>
2025-11-20 18:26:31 -0800

    Created frontend dockerfile, adjusted API_URL in axios.js to use container port instead of localhost

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/stories";
+const API_URL = "http://0.0.0.0:8080/api/stories";
 export const getNewsStories = async () => {
   const response = await axios.get(API_URL);
   return response.data;
